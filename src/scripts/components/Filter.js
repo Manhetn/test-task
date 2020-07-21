a7129c6fa2269130cc6228bc35f93608d17ac6a5
Ilya Shestakov <manhetnmd@gmail.com>
2020-07-21 19:35:25 +0300

    Refactoring 2

--- src/scripts/components/Filter.js
+++ src/scripts/components/Filter.js
@@ -7,13 +7,17 @@ export default class Filter extends BaseComponent {
 
 
   setListeners() {
+    console.log('setFilter!!!!!!!!!!!!!!!!!!!!!!')
       this._setHandlers([
-        { element: this.elements.input, event: 'input', handler: this._filter.bind(this) },
+        { element: this.elements.input,
+          event: 'input',
+          handler: this._filter.bind(this)
+        },
       ]);
     }
 
   removeListeners() {
-
+    console.log('removeFilter!!!!!!!!!!!!!!!!!!!!!!')
     this._removeHandlers([
       { element: this.block,
         event: 'input',
@@ -26,13 +30,74 @@ export default class Filter extends BaseComponent {
     const { storage, friends, pagination } = this._dependencies;
     const friendsArray = JSON.parse(storage.getPropertyValue('friends'));
     const filteredFriends = friendsArray.filter(friend => {
-       return  friend.email.includes(this.elements.input.value) || friend.first_name.includes(this.elements.input.value) || friend.last_name.includes(this.elements.input.value)
+       return friend.email.includes(this.elements.input.value) || friend.first_name.includes(this.elements.input.value) || friend.last_name.includes(this.elements.input.value)
     })
+    // this.removeListeners();
+    // pagination.removeListeners();
     friends.clearList();
     pagination.clearPagination();
-    // friends._renderFriends(filteredFriends);
     friends.renderFriends(filteredFriends);
+    // pagination.setListeners();
+    // this.setListeners();
   }
+
+  // _filter() {
+  //   const { storage, friends, pagination } = this._dependencies;
+  //   const friendsArray = JSON.parse(storage.getPropertyValue('friends'));
+  //   let filteredFriends = [];
+  //   if (this.elements.input.value !== '0') {
+  //     // this.removeListeners();
+  //     // pagination.removeListeners();
+  //     // pagination.clearPagination();
+  //     // friends.clearList();
+  //     // console.log('yo')
+  //     filteredFriends = friendsArray.filter(friend => {
+  //       return  friend.email.includes(this.elements.input.value) || friend.first_name.includes(this.elements.input.value) || friend.last_name.includes(this.elements.input.value)
+  //     });
+  //     this.removeListeners();
+  //     pagination.removeListeners();
+
+  //     pagination.clearPagination();
+  //     friends.clearList();
+
+  //     console.log(filteredFriends)
+
+
+  //     friends.renderFriends(filteredFriends);
+  //     // this.removeListeners();
+
+
+  //   } else {
+  //     pagination.removeListeners();
+  //     // this.removeListeners();
+  //     pagination.clearPagination();
+  //     friends.clearList();
+  //     friends.renderFriends(filteredFriends);
+  //     // this.removeListeners();
+  //     // pagination.removeListeners();
+  //     // pagination.clearPagination();
+  //     // this.removeListeners();
+  //     // friends.clearList();
+  //     // friends.renderFriends(filteredFriends);
+  //   }
+
+
+
+  //   // friends._renderFriends(filteredFriends);
+
+  // }
+
+  // _filter() {
+  //   const { storage, friends, pagination } = this._dependencies;
+  //   const friendsArray = JSON.parse(storage.getPropertyValue('friends'));
+  //   const filteredFriends = friendsArray.filter(friend => {
+  //      return  friend.email.includes(this.elements.input.value) || friend.first_name.includes(this.elements.input.value) || friend.last_name.includes(this.elements.input.value)
+  //   })
+  //   friends.clearList();
+  //   pagination.clearPagination();
+  //   // friends._renderFriends(filteredFriends);
+  //   friends.renderFriends(filteredFriends);
+  // }
 
 
 }
--- src/scripts/components/Friends.js
+++ src/scripts/components/Friends.js
@@ -6,42 +6,43 @@ export default class Friends extends BaseComponent {
     this.data = parameters.data;
   }
 
-  _getFriends() {
-    const { apiFriends, storage, pagination } = this._dependencies;
+  getFriends() {
+    const { apiFriends, storage, notFound, filter, pagination } = this._dependencies;
 
     apiFriends
       .getFriends()
       .then(res => {
         storage.setPropertyValue('friends', JSON.stringify(res.data));
-        this.renderFriends(res.data);
-
+        if (res.data.length !== 0 ) {
+          this.renderFriends(res.data);
+          filter.setListeners();
+          pagination.setListeners();
+        } else {
+          this._addPages(notFound.createNoFriendFound());
+        }
       });
   }
 
   renderFriends(arrayFriends) {
-    const { pagination, notFound } = this._dependencies;
-    const numberFriendsOnThePage = this.data.friendsOnThePage;
+      const { pagination, notFound, filter } = this._dependencies;
+      const numberFriendsOnThePage = this.data.friendsOnThePage;
 
-    if (arrayFriends.length !== 0) {
-      for (let i = 0; i <= arrayFriends.length; i = i + numberFriendsOnThePage) {
-        let endNumber = i + +numberFriendsOnThePage;
-        const numberPage = this._getPageNumber(i, numberFriendsOnThePage);
-        const arrayFriendOnPage = arrayFriends.slice(i, endNumber)
-
-        this._addPages(this._createPage(numberPage));
-
-        const page = document.querySelector(`.friends__page-${numberPage}`);
-
-        this._addFriends(arrayFriendOnPage, page)
-        pagination.addPageNumber(numberPage);
+      if(arrayFriends.length !== 0) {
+        for (let i = 0; i < arrayFriends.length; i = i + numberFriendsOnThePage) {
+          const endNumber = i + numberFriendsOnThePage;
+          const numberPage = this._getPageNumber(i, numberFriendsOnThePage);
+          const arrayFriendOnPage = arrayFriends.slice(i, endNumber);
+          this._addPages(this._createPage(numberPage));
+          const page = document.querySelector(`.friends__page-${numberPage}`);
+          this._addFriends(arrayFriendOnPage, page);
+          pagination.addPageNumber(numberPage);
+        }
+        pagination.activatFirstPageNumber();
+        this._visibleFirstPage();
+      } else {
+        this._addPages(notFound.createNoFriendFound());
       }
-      pagination.activatFirstPageNumber();
-      pagination.setListeners();
-      this._visibleFirstPage();
-    } else {
-      this._addPages(notFound.createNoFriendFound());
     }
-  }
 
   _visibleFirstPage() {
     const firsPage = this.elements.friendsList.firstElementChild;
@@ -51,7 +52,7 @@ export default class Friends extends BaseComponent {
 
   // получаем номер страницы
   _getPageNumber(number, numberOfFriendsOnThePage) {
-    return Math.ceil((+number + 1) / numberOfFriendsOnThePage);
+    return Math.ceil((number +1) / numberOfFriendsOnThePage)
   }
 
   // создаем страницу для друзей
@@ -75,7 +76,6 @@ export default class Friends extends BaseComponent {
 
   // отоброжаем страницы в friend list
   _addPages(page){
-    console.log(page)
     this.elements.friendsList.insertAdjacentHTML('beforeend', page);
   }
 
